26d3011e5da30aea7e77c72a82c3b1cc4d0ca3ce
unknown <hashmiabdulmajid@gmail.com>
2024-12-19 11:51:03 +0530

    chat app done be

--- server.js
+++ server.js
@@ -1,10 +1,8 @@
 const express = require("express");
 const app = express();
-const {Server} = require('socket.io');
-const userSocketMap = {};
-const http = require('http');
-const server  = http.createServer(app);
-
+const { Server } = require("socket.io");
+const http = require("http");
+const server = http.createServer(app);
 const dbconnect = require("./config/db.js");
 require("dotenv").config();
 const PORT = process.env.PORT;
@@ -13,79 +11,41 @@ dbconnect();
 app.use(express.json());
 const cors = require("cors");
 
-const io = new Server(server,{
-
-  cors:{
-    origin:['http://localhost:3000',"https://personalwebsite-omega-two.vercel.app"],
+const io = new Server(server, {
+  cors: {
+    origin: [
+      "http://localhost:3000",
+      "https://personalwebsite-omega-two.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS", "HEAD"],
-  }
+  },
 });
 
-// Disconnect all clients and clear all rooms
-// io.sockets.sockets.forEach(socket => {
-//   socket.disconnect(true);  // Disconnect each socket
-// });
-
-
-io.on('connection',(socket)=>{
+io.on("connection", (socket) => {
   
-
-  socket.on("joinRoom",(userId,room)=>{
-   const rooms = Object.keys(socket.rooms);
-    if (!rooms.includes(room)) {
-    socket.join(room);
+  // io.sockets.sockets.forEach(socket => {
+  //   socket.disconnect(true);  // Disconnect each socket
+  // });
+  socket.on("joinRoom", (userId) => {
     console.log(userId);
-    
-     userSocketMap[userId] = socket.id; 
-    
-    
-      const status = "online";
-      io.to(room).emit("isOnline",{userId,status});
-    }
-  })
-
-
-
-socket.on('leaveRoom',(userId,room)=>{
-  console.log("room uck",userId,room);
-  const status = "offline"
-  io.to(room).emit('userLeft',{userId,status});
-socket.leave(room);
-
-
-
-})
-  
-
-  socket.on('sendMessage',({touserId,message})=>{
+    socket.join(userId);
     console.log(io.sockets.adapter.rooms);
-
- 
-      
-      const recipientSocketId = userSocketMap[touserId];
-
-    console.log(touserId,message);
-    
-    
-
-    io.to(recipientSocketId).emit('recieveMessage',{message});
-
-  })
-  socket.on('disconnect', () => {
-    // Remove user from the map when they disconnect
-    
-    console.log(socket.id)
-    for (let userId in userSocketMap) {
-      if (userSocketMap[userId] === socket.id) {
-       
-        delete userSocketMap[userId];
-        console.log(`User with ID ${userId} disconnected`);
-        break;
-      }
-    }
   });
 
-})
+  
+
+  socket.on("sendMessage", ({ use, msg }) => {
+
+    const message = msg;
+    console.log(use, message);
+    socket.to(use).emit("recieveMessage", { message });
+  });
+  socket.on("disconnect", (reason) => {
+    console.log(`User disconnected: ${socket.id}, Reason: ${reason}`);
+    // Perform any cleanup here, like removing the user from active lists
+  });
+
+});
 
 app.use(
   cors({
